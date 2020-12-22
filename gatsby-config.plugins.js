1deb3a36dc7ea2471efc69d936ba41dbbd2fe3cf
Nick Chinsen <nicholaschinsen@gmail.com>
2020-12-22 17:04:22 -0500

    changed region for deploy

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -16,8 +16,8 @@ module.exports = [
   {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      name: 'Rolwin Reevan',
-      short_name: 'Rolwin Reevan',
+      name: 'Nick Chinsen',
+      short_name: 'Nick Chinsen',
       start_url: '/',
       background_color: '#304CFD',
       theme_color: '#304CFD',
